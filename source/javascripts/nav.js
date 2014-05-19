42eb0f347aacb867b8bf638607a4e78a243eb279
Salazar177 <Ro@host-245-157.pubnet.pdx.edu>
2014-05-19 14:57:24 -0700

    Messing around with javascript debugging
    Things to do:
    -Relink woofoo in only catering.html and contact.html
    -Fix bootstrap.css link

--- source/javascripts/nav.js
+++ source/javascripts/nav.js
@@ -1,7 +1,22 @@
-$(window).resize(function(){
+/*
+$(window).load(function(){
+	if ($(window).width() <= 767){
+		// do something here
+		$('.navUIButtonContainer').removeClass('hidden');
+		$('.navLinks').addClass('hidden');
+	}
+	$(window).resize(function(){
 	if ($(window).width() <= 767){	
 		// do something here
-		$('.navUIButton').removeClass('hidden');
-/* 		$('') */
-	}	
+		$('.navUIButtonContainer').removeClass('hidden');
+		$('.navLinks').addClass('hidden');
+	}
+	if ($(window).width() >= 767){	
+		// do something here
+		$('.navUIButtonContainer').addClass('hidden');
+		$('.navLinks').removeClass('hidden');
+	}
 });
+
+});
+*/
