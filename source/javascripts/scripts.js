ec256ba3ac63718b8169384aea5cbfee26254ff6
Salazar177 <Ro@host-245-247.pubnet.pdx.edu>
2014-05-21 13:12:24 -0700

    Messing with Javascript for navlinks
    Trying to make active nav links.

---
+++ source/javascripts/scripts.js
@@ -0,0 +1,40 @@
+/*
+$(window).load(function(){
+	if(window.location.pathname == '../menu.html'){
+		// do something here
+		$('.menuLinks').addClass('red');
+		console.log("Make link active.");
+	};
+});
+*/
+
+/*
+$('.navLinks ul li a').each(function(){
+
+var path = window.location.href;
+var loc = path.substring(path.lastIndexOf('/')+1);
+var url = $(this).attr('href');
+
+if(url == loc){
+$(this).parent().addClass('red');
+};
+});
+*/
+
+/* var pathname = window.location.pathname; */
+
+/*
+$(window).load(function(){
+	var pathname = window.location.pathname;
+	if (window.location.pathname == '../index.html'){
+		console.log("Whatever");
+	}
+});
+*/
+
+$('.navLink').each(function() {
+	if ($(this).attr('href')  ==  window.location.pathname) {
+/* 	  $(this).addClass('red'); */
+	  console.log("Whatever");
+	}
+});
