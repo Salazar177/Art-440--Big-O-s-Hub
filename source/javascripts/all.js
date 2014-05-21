02df18834bada746c86455adfae8c58ad77e1fa4
Salazar177 <Ro@host-245-247.pubnet.pdx.edu>
2014-05-21 14:06:08 -0700

    Added JS for active link

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,3 @@
 //= require bootstrap.min
 //= require nav
-//= scripts
+//= require scripts
--- source/javascripts/nav.js
+++ source/javascripts/nav.js
@@ -5,16 +5,15 @@ $(window).load(function(){
 		$('.navLinks').addClass('hidden');
 	}
 	$(window).resize(function(){
-	if ($(window).width() <= 767){	
-		// do something here
-		$('.navUIButtonContainer').removeClass('hidden');
-		$('.navLinks').addClass('hidden');
-	}
-	if ($(window).width() >= 767){	
-		// do something here
-		$('.navUIButtonContainer').addClass('hidden');
-		$('.navLinks').removeClass('hidden');
-	}
-});
-
+		if ($(window).width() <= 767){	
+			// do something here
+			$('.navUIButtonContainer').removeClass('hidden');
+			$('.navLinks').addClass('hidden');
+		}
+		if ($(window).width() >= 767){	
+			// do something here
+			$('.navUIButtonContainer').addClass('hidden');
+			$('.navLinks').removeClass('hidden');
+		}
+	});
 });
